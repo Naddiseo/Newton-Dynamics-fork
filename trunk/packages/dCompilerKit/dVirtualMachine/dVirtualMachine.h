5bf6e7b86be44f7fd40b6441ae957b59fe15d040
JerezJulio0@gmail.com <JerezJulio0@gmail.com@b7a2f1d6-d59d-a8fe-1e9e-8d4888b32692>
2011-09-18 16:51:14 +0000

    git-svn-id: http://newton-dynamics.googlecode.com/svn@797 b7a2f1d6-d59d-a8fe-1e9e-8d4888b32692

--- trunk/packages/dCompilerKit/dVirtualMachine/dVirtualMachine.h
+++ trunk/packages/dCompilerKit/dVirtualMachine/dVirtualMachine.h
@@ -12,7 +12,7 @@ class dVirtualMachine
 		mov,		// move		Rn0, Rn1			Rn0 = Rn1
 
 		// arithmetic
-		addi,		// addi 	Rn0, Rn1, immediate	Rn0 = Rn1 + signed (immediate)
+		addi,		// addi 	Rn0, Rn1, imm32		Rn0 = Rn1 + signed (imm32)
 		add,		// add  	Rn0, Rn1			Rn0 = Rn0 + Rn1
 		sub,		// sub  	Rn0, Rn1			Rn0 = Rn0 - Rn1
 		mul,		// mul  	Rn0, Rn1			Rn0 = lowpart (Rn0 * Rn1) ; Rn1 = upperpart (Rn0 * Rn1)
@@ -27,38 +27,39 @@ class dVirtualMachine
 		not,		// xor		Rn0, Rn1			Rn0 = ~Rn1
 
 		// shift 
-		sll,		// sll		Rn0, Rn1, Rn2		Rn0 = Rn0 << Rn1	
-		srl,		// srl		Rn0, Rn1, Rn2		Rn0 = Rn0 >> Rn1	
+		sll,		// sll		Rn0, Rn1			Rn0 = Rn0 << Rn1	
+		srl,		// srl		Rn0, Rn1			Rn0 = Rn0 >> Rn1	
 
 		// load 
-		loadb,		// loadb	Rn0, immediate[Rn1]	Rn0 = byte at memory [Rn1 + immediate]	
-		loadw,		// loadq	Rn0, immediate[Rn1]	Rn0 = word at memory [Rn1 + immediate]	
-		loadd,		// loadd	Rn0, immediate[Rn1]	Rn0 = dword at memory [Rn1 + immediate]
+		loadb,		// loadb	Rn0, imm32[Rn1]		Rn0 = byte at memory [Rn1 + imm32]	
+		loadw,		// loadq	Rn0, imm32[Rn1]		Rn0 = word at memory [Rn1 + imm32]	
+		loadd,		// loadd	Rn0, imm32[Rn1]		Rn0 = dword at memory [Rn1 + imm32]
 
 		// store 
-		storeb,		// storeb	Rn0, Rn1, immediate	byte at memory [Rn1 + immediate] = Rn0
-		storew,		// storeq	Rn0, Rn1, immediate	word at memory [Rn1 + immediate] = Rn0
-		stored,		// stored	Rn0, Rn1, immediate	dword at memory [Rn1 + immediate] = Rn0
+		storeb,		// storeb	Rn0, Rn1, imm32		byte at memory [Rn1 + imm32] = Rn0
+		storew,		// storeq	Rn0, Rn1, imm32		word at memory [Rn1 + imm32] = Rn0
+		stored,		// stored	Rn0, Rn1, imm32		dword at memory [Rn1 + imm32] = Rn0
 
 		// conditional jumps
-		beq,		// beq		Rn0, Rn1, immediate  	if (Rn0 == Rn1) PC = PC + immediate  
-		bne,		// bneq		Rn0, Rn1, immediate  	if (Rn0 != Rn1) PC = PC + immediate  
-		blt,		// blt		Rn0, Rn1, immediate  	if (Rn0 < Rn1) PC = PC + immediate  (signed comparison)
-		ble,		// ble		Rn0, Rn1, immediate  	if (Rn0 <= Rn1) PC = PC + immediate  (signed comparison)
-		bgt,		// bgt		Rn0, Rn1, immediate  	if (Rn0 > Rn1) PC = PC + immediate  (signed comparison)
-		bget,		// bget		Rn0, Rn1, immediate  	if (Rn0 >= Rn1) PC = PC + immediate  (signed comparison)
+		beq,		// beq		Rn0, Rn1, imm32  	if (Rn0 == Rn1) PC = PC + imm32  
+		bne,		// bneq		Rn0, Rn1, imm32  	if (Rn0 != Rn1) PC = PC + imm32  
+		blt,		// blt		Rn0, Rn1, imm32  	if (Rn0 < Rn1) PC = PC + imm32  (signed comparison)
+		ble,		// ble		Rn0, Rn1, imm32  	if (Rn0 <= Rn1) PC = PC + imm32  (signed comparison)
+		bgt,		// bgt		Rn0, Rn1, imm32  	if (Rn0 > Rn1) PC = PC + imm32  (signed comparison)
+		bget,		// bget		Rn0, Rn1, imm32  	if (Rn0 >= Rn1) PC = PC + imm32  (signed comparison)
 
-		// call and return 
-		call,		// call		Rn0, immediate			Rn0 = [pc], pc = immediate
-		callr,		// call		Rn0, Rn1				Rn0 = [pc], pc = Rn1
-		syscall,	// syscall	Rn0, Rn1, immediate		execute a system call
-		jump,		// jump		Rn0						pc = Rn0
+		// call / return 
+		call,		// call		Rn0, imm32			Rn0 = Rn0 - 1, [Rn0] = pc + 3, pc = imm32
+		callr,		// call		Rn0, Rn1			Rn0 = Rn0 - 1, [Rn0] = pc + 3, pc = Rn1
+		ret,		// ret		rn0					pc = [Rn0], Rn0 = Rn0 + 1  
+		syscall,	// syscall	Rn0, Rn1, imm32		execute a system call
+		jump,		// jump		Rn0					pc = Rn0
 
 		// frame pointer 
-		enter,		// enter	Rn0, Rn1, immediate		[Rn1] = Rn0, Rn0 = Rn1, Rn1 = Rn1 + immediate	
-		exit,		// exit		Rn0, Rn1				Rn1 = Rn0, Rn0 = [Rn1]
-		push,		// push		Rn0, registerMask		tmp = rn0; i = 0; mask = registenMask; while (mask & 1) {[tmp] = Rn(i); tmp += 4}; rn0 = tmp; 
-		pop,		// pop		Rn0, registerMask		tmp = rn0; i = 0; mask = registenMask; while (mask & (1<<15)) {tmp -= 4; Rn(i) = [tmp];}
+		enter,		// enter	Rn0, Rn1, imm32		[Rn1] = Rn0, Rn0 = Rn1, Rn1 = Rn1 + imm32	
+		exit,		// exit		Rn0, Rn1			Rn1 = Rn0, Rn0 = [Rn1]
+		push,		// push		Rn0, imm32mask		tmp = rn0; i = 0; mask = registenMask; while (mask & 1) {[tmp] = Rn(i); tmp -= 1}; rn0 = tmp; 
+		pop,		// pop		Rn0, imm32mask		tmp = rn0; i = 0; mask = registenMask; while (mask & (1<<15)) {tmp += 1; Rn(i) = [tmp];}
 
 
 		nop,		// do nothing 
@@ -72,18 +73,17 @@ class dVirtualMachine
 
 	union dOpCode
 	{
+		short m_bytecode;
 		struct dType0
 		{
 			unsigned	m_opcode	:  6;
 			unsigned	m_reg0		:  5;
 			unsigned	m_reg1		:  5;
-			int			m_immediate : 16;
 		};
 		struct dType1
 		{
 			unsigned	m_opcode	:  6;
 			unsigned	m_reg0		:  5;
-			int			immediate	: 21;
 		};
 	};
 
