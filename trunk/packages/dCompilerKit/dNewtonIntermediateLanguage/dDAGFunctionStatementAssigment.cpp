1a1c54c616d2c5f7623c4ee3ea780cddc7882f95
jerezjulio@sbcglobal.net <jerezjulio@sbcglobal.net@b7a2f1d6-d59d-a8fe-1e9e-8d4888b32692>
2011-10-09 15:10:47 +0000

    git-svn-id: http://newton-dynamics.googlecode.com/svn@909 b7a2f1d6-d59d-a8fe-1e9e-8d4888b32692

---
+++ trunk/packages/dCompilerKit/dNewtonIntermediateLanguage/dDAGFunctionStatementAssigment.cpp
@@ -0,0 +1,35 @@
+/* Copyright (c) <2009> <Newton Game Dynamics>
+*
+* This software is provided 'as-is', without any express or implied
+* warranty. In no event will the authors be held liable for any damages
+* arising from the use of this software.
+*
+* Permission is granted to anyone to use this software for any purpose,
+* including commercial applications, and to alter it and redistribute it
+* freely
+*/
+
+#include "dDirectAcyclicgraphNode.h"
+#include "dDAGFunctionStatementAssigment.h"
+
+dInitRtti(dDAGFunctionStatementAssigment);
+
+dDAGFunctionStatementAssigment::dDAGFunctionStatementAssigment(dList<dDirectAcyclicgraphNode*>& allNodes, dDAGExpressionNodeVariable* const leftVariable, dDAGExpressionNode* const expression)
+	:dDAGFunctionStatement(allNodes)
+	,m_expression(expression)
+	,m_leftVariable(leftVariable)
+{
+	_ASSERTE (m_expression);
+	_ASSERTE (m_leftVariable);	
+	m_expression->AddRef();
+	m_leftVariable->AddRef();
+}
+
+
+dDAGFunctionStatementAssigment::~dDAGFunctionStatementAssigment()
+{
+	m_expression->Release();
+	m_leftVariable->Release();
+}
+
+
